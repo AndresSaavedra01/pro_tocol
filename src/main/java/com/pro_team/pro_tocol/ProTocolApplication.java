aef6960279a5cbd6c22bd0911406f2eb91a7120e
Andressaavedra01 <andres.saavedra01@uceva.edu.co>
2026-03-09 17:00:04 -0500

    reconfiguracion del proyecto

---
+++ src/main/java/com/pro_team/pro_tocol/ProTocolApplication.java
@@ -0,0 +1,13 @@
+package com.pro_team.pro_tocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTocolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTocolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro_team/pro_tocol/ProTocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro_team.pro_tocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProTocolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
